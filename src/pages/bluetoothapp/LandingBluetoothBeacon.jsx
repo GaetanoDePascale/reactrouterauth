0276b0d73015b003d93e0cc95b9221260bbaa6e0
g.depascale <g.depascale@nexsoft.it>
2023-01-12 10:44:04 +0100

    chrome://flags message

--- src/pages/bluetoothapp/LandingBluetoothBeacon.jsx
+++ src/pages/bluetoothapp/LandingBluetoothBeacon.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 const LandingBluetoothBeacon = () => {
     const [beaconData, setBeaconData] = useState(null);
     const [error, setError] = useState(undefined);
+    const [solution, setSolution] = useState(undefined);
 
     useEffect(() => {
         // Verifica se il browser supporta l'API Web Bluetooth
@@ -30,6 +31,16 @@ const LandingBluetoothBeacon = () => {
             };
         } catch (error) {
             setError(error);
+            if (error.message === 'navigator.bluetooth.requestLEScan is not a function') {
+                setSolution(
+                    <>
+                        <br /><br /><b>Soluzione:</b><br />
+                        requestLEScan is behind a flag in Chrome (chrome://flags/#enable-experimental-web-platform-features).
+                        <br />
+                        See <a href="https://github.com/WebBluetoothCG/web-bluetooth/blob/main/implementation-status.md#scanning-api" target={'_blank'}>https://github.com/WebBluetoothCG/web-bluetooth/blob/main/implementation-status.md#scanning-api</a>.
+                    </>
+                )
+            }
         };
 
     }, []);
@@ -45,7 +56,15 @@ const LandingBluetoothBeacon = () => {
                 ) : (
                     <p>Cercando beacon...</p>
                 )
-                : <p><b>Errore:</b> {error.message}</p>}
+                :
+                <p>
+                    <b>Errore:</b> {error.message}
+                    {solution !== undefined ?
+                        solution
+                        :
+                        <></>
+                    }
+                </p>}
         </div>
     );
 };
